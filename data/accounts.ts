6927947089b10dc7beb68da73453891e21196f26
hoaithanhsp <tolyhogroup@gmail.com>
2026-02-01 07:33:27 +0700

    Update accounts.ts

--- data/accounts.ts
+++ data/accounts.ts
@@ -16,8 +16,9 @@ export interface VIPAccount {
 
 export const VIP_ACCOUNTS: VIPAccount[] = [
     { username: "admin", password: "Hoaithanh@2", displayName: "Quản trị viên" },
-    { username: "tranhuyenthnd@gmail.com", password: "123456", displayName: "Tran Huyen" },
-    { username: "haloch2010@gmail.com", password: "123456", displayName: "GV1" },
+    { username: "tranhuyenthnd@gmail.com", password: "123456", displayName: "GV1" },
+    { username: "haloch2010@gmail.com", password: "123456", displayName: "GV2" },
+    { username: "baubinna@gmail.com", password: "123456", displayName: "GV3" },
     // Thêm tài khoản mới ở đây, mỗi dòng 1 account:
     // { username: "teacher2", password: "abcdef", displayName: "Cô Trần Thị B" },
 ];
@@ -55,6 +56,7 @@ export const getLoggedInUser = (): { username: string; displayName: string } | n
 export const logout = () => {
     localStorage.removeItem('vip_user');
 };
+
 
 
 
