263435590bdbb5ff102e7d935a0257b557aab535
hoaithanhsp <tolyhogroup@gmail.com>
2026-02-01 08:17:49 +0700

    Update accounts.ts

--- data/accounts.ts
+++ data/accounts.ts
@@ -17,6 +17,7 @@ export interface VIPAccount {
 export const VIP_ACCOUNTS: VIPAccount[] = [
     { username: "admin", password: "Hoaithanh@2", displayName: "Quản trị viên" },
     { username: "admin", password: "admin", displayName: "GV" },
+    { username: "admin1", password: "admin", displayName: "Giáo viên" },
     { username: "tranhuyenthnd@gmail.com", password: "123456", displayName: "GV1" },
     { username: "haloch2010@gmail.com", password: "123456", displayName: "GV2" },
     { username: "baubinna@gmail.com", password: "123456", displayName: "GV3" },
@@ -57,6 +58,7 @@ export const getLoggedInUser = (): { username: string; displayName: string } | n
 export const logout = () => {
     localStorage.removeItem('vip_user');
 };
+
 
 
 
