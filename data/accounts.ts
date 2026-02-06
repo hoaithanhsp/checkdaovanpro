bcbf44f7b8e4158e6e006452599a4aefa4be77ab
hoaithanhsp <tolyhogroup@gmail.com>
2026-02-06 15:25:31 +0700

    Update accounts.ts

--- data/accounts.ts
+++ data/accounts.ts
@@ -20,6 +20,8 @@ export const VIP_ACCOUNTS: VIPAccount[] = [
     { username: "admin1", password: "admin", displayName: "Giáo viên" },
     { username: "tranhuyenthnd@gmail.com", password: "123456", displayName: "GV" },
     { username: "phamthihiep.kd@elc.vn", password: "SKKN100", displayName: "GV" },
+    { username: "phantruongduy1981@gmail.com", password: "SKKN100", displayName: "GV" },
+    { username: "Belam088@gmail.com", password: "SKKN100", displayName: "GV" },
     { username: "duongle2512@gmail.com", password: "SKKN100", displayName: "GV" },
     { username: "minhtrang", password: "SKKN100", displayName: "GV" },
     { username: "petnguyenhoang@gmail.com", password: "SKKN100", displayName: "GV" },
@@ -88,6 +90,7 @@ export const getLoggedInUser = (): { username: string; displayName: string } | n
 export const logout = () => {
     localStorage.removeItem('vip_user');
 };
+
 
 
 
